d25d6bfcab58e1768b14fc4f11fcfbbb939a5597
ilanbenyosef <ilanbenyosef@gmail.com>
2020-02-16 10:06:52 +0200

    wip

--- server/plugins/mongoose.js
+++ server/plugins/mongoose.js
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose'
 mongoose.Promise = global.Promise;
-const password = 'admin'
-const mongoAtlasUrl = `mongodb+srv://admin:${password}@cluster0-dvepl.azure.mongodb.net/test?retryWrites=true&w=majority`
+const password = 'admin';
+const mongoAtlasUrl = `mongodb://ilanben:xsw23edc@ds131721.mlab.com:31721/trippo`
+
+// const mongoAtlasUrl = `mongodb+srv://admin:${password}@cluster0-dvepl.azure.mongodb.net/test?retryWrites=true&w=majority`
 mongoose.connect(mongoAtlasUrl);
 
 var db = mongoose.connection;

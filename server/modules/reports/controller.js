3520f29790d4601ba8049ed0e8ae5aa6fab4f7be
ilanbenyosef <ilanbenyosef@gmail.com>
2020-03-23 14:53:40 +0200

    wip

--- server/modules/reports/controller.js
+++ server/modules/reports/controller.js
@@ -27,27 +27,8 @@ export async function deleteReportById(id) {
 export async function saveReport(reportData) {
   const options = { upsert: true, new: true, setDefaultsOnInsert: true };
   console.warn("reportData1111111111111",reportData)
-  let arr = Object.keys(reportData)[0];
-  arr = arr.substring(0,arr.length-2).substring(1).split(', ');
-  arr = arr.map(i=> {
-    return i.substring(0,i.length-1).substring(1)
-  });
-
-    const o = {
-      reporterId: arr[1],
-      reporterPhone: arr[2],
-      firstName: arr[3],
-      lastName: arr[4],
-      plateNumber: arr[5],
-      reporterEmail: arr[6],
-      comments: arr[7],
-      json: JSON.stringify(reportData)
-    }
-
-    console.warn("reportData after parsing:",o)
-
-
-
+  let o =this.parseReportFromAppInventor(reportData)
+  
   try {
     let newReport;
     reportData.json = JSON.stringify(o)
@@ -67,4 +48,26 @@ export async function saveReport(reportData) {
     ctx.body = error;
     ctx.status = 500
   }
+}
+
+const parseReportFromAppInventor = (reportData)=>{
+  let arr = Object.keys(reportData)[0];
+  arr = arr.substring(0,arr.length-2).substring(1).split(', ');
+  arr = arr.map(i=> {
+    return i.substring(0,i.length-1).substring(1)
+  });
+
+    const o = {
+      reporterId: arr[1],
+      reporterPhone: arr[2],
+      firstName: arr[3],
+      lastName: arr[4],
+      plateNumber: arr[5],
+      reporterEmail: arr[6],
+      comments: arr[7],
+      json: JSON.stringify(reportData)
+    }
+
+    console.warn("reportData after parsing:",o)
+    return o;
 }
